9fc1b1b09b87fbae0ffe32f39c77f630709ad79b
Vladislav <bombardire54@gmail.com>
2021-10-23 21:36:19 +0300

    Created basic components on login page

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, TextInput } from 'react-native';
 
-export default function App() {
+const CustomButton = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <button style={{
+              backgroundColor: 'cadetblue',
+              border: 'none',
+              padding: '10px',
+              width: '80%'
+         }} onClick={() => alert('hello')}>Login</button>
+  )
+}
+
+
+export default class App extends React.PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasFocus: false
+    };
+  }
+
+  checkEmail(e) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (regex.test(e.target.value)) {
+
+    }
+  }
+
+  checkPassword(e) {
+    const regex = /^\w{8,}$/
+    if (regex.test(e.target.value)) {
+
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput style={styles.input}
+                   maxLength='30'
+                   autoComplete='email'
+                   placeholder='Login'
+                   onChange={this.checkEmail.bind(this)} /> 
+        <TextInput style={styles.input}
+                   maxLength='30'
+                   secureTextEntry={true}
+                   placeholder='Password'
+                   onChange={this.checkPassword.bind(this)} /> 
+        <CustomButton style={styles.login_btn} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#333',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    border: 'none',
+    marginBottom: '10px',
+    color: '#fff'
+  },
+  login_btn: {
+    border: 'none',
+    color: '#fff',
+    backgroundColor: 'cadetblue'
+  }
 });
